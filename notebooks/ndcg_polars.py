88f1ace2679004038e996d7e6ae9d09cd8dc1b6c
Markus Schepke <markus.schepke@wolt.com>
2023-04-14 22:51:51 +0300

    first actual calculation of NDCG

--- notebooks/ndcg_polars.py
+++ notebooks/ndcg_polars.py
@@ -15,6 +15,7 @@
 
 # %%
 import polars as pl
+from sklearn.metrics import ndcg_score
 import turicreate as tc
 
 # %load_ext nb_black
@@ -38,17 +39,22 @@ ratings = (
 )
 
 # %%
-train_test = ratings.partition_by("is_test_row", as_dict=True)
+train_test = ratings.partition_by(
+    "is_test_row",
+    as_dict=True,
+)
 data_train = train_test[False]
 data_train.drop_in_place("is_test_row")
+data_train = data_train.sort("bgg_user_name", "bgg_id")
 data_test = train_test[True]
 data_test.drop_in_place("is_test_row")
+data_test = data_test.sort("bgg_user_name", "bgg_id")
 data_train.shape, data_test.shape
 
 # %%
 data_train.write_csv("ratings_train.csv")
 data_test.write_csv("ratings_test.csv")
-del train_test, data_train, data_test
+del train_test, data_train
 
 # %%
 ratings = tc.SFrame.read_csv("ratings_train.csv")
@@ -64,3 +70,28 @@ model = tc.ranking_factorization_recommender.create(
     max_iterations=10,
     verbose=True,
 )
+
+# %%
+y_true = data_test["bgg_user_rating"].to_numpy().reshape((-1, 25))
+y_true.shape
+
+
+# %%
+def recommend_from_pl(data):
+    user = data["bgg_user_name"][0]
+    sa = model.recommend(
+        users=[user],
+        items=data["bgg_id"].to_numpy(),
+        k=len(data),
+        exclude_known=False,
+    ).sort("bgg_id")["score"]
+    assert len(data) == len(sa)
+    return pl.DataFrame(data={"score": sa.to_numpy()})
+
+
+# %%
+recommendations = data_test.groupby("bgg_user_name").apply(recommend_from_pl)
+y_score = recommendations.to_numpy().reshape((-1, 25))
+
+# %%
+ndcg_score(y_true, y_score)
