7a65c4b82fc39031031d96442f365e34f2f749b1
Markus Schepke <markus.schepke@wolt.com>
2023-04-24 21:35:20 +0300

    added ndcg_train_test.py

---
+++ notebooks/ndcg_train_test.py
@@ -0,0 +1,58 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import polars as pl
+
+# %load_ext nb_black
+# %load_ext lab_black
+
+# %%
+THRESHOLD_POWER_USERS = 100
+NUM_LABELS = 25
+TOP_K = 10
+
+# %%
+ratings = (
+    pl.scan_ndjson("../../board-game-data/scraped/bgg_RatingItem.jl")
+    .filter(pl.col("bgg_user_rating").is_not_null())
+    .select(
+        "bgg_id",
+        "bgg_user_name",
+        "bgg_user_rating",
+        (
+            (pl.col("bgg_id").count().over("bgg_user_name") >= THRESHOLD_POWER_USERS)
+            & (pl.arange(0, pl.count()).shuffle().over("bgg_user_name") < NUM_LABELS)
+        ).alias("is_test_row"),
+    )
+    .collect()
+)
+
+# %%
+train_test = ratings.partition_by(
+    "is_test_row",
+    as_dict=True,
+)
+data_train = train_test[False]
+data_train.drop_in_place("is_test_row")
+data_train = data_train.sort("bgg_user_name", "bgg_id")
+data_test = train_test[True]
+data_test.drop_in_place("is_test_row")
+data_test = data_test.sort("bgg_user_name", "bgg_id")
+data_train.shape, data_test.shape
+
+# %%
+data_train.write_csv("ratings_train.csv")
+data_test.write_csv("ratings_test.csv")
